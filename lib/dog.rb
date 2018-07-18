0ed25676806fa10c00ecf3975f4142bc629f666f
Courtney Haltiwanger <chalt0319@gmail.com>
2018-07-18 13:25:37 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -28,7 +28,7 @@ attr_reader :id
   end
 
   def self.create(name:, breed:)
-    new_dog = Dog.new(name, breed)
+    new_dog = Dog.new(name: name, breed: breed)
     new_dog.save
     new_dog
   end
