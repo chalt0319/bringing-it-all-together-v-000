3675247967c44f81e7ab0f2050a1e0e0dfd6c632
Courtney Haltiwanger <chalt0319@gmail.com>
2018-07-18 14:20:40 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -58,8 +58,10 @@ attr_reader :id
     Dog.new(id: row[0], name: row[1], breed: row[2])
   end
 
-  def self.find_by_name
-
+  def self.find_by_name(name)
+    sql = "SELECT * FROM dogs WHERE name = ?"
+    row = DB[:conn].execute(sql)[0]
+    Dog.new_from_db(row)
   end 
 
 end
